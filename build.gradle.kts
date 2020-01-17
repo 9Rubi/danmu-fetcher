67e8f7c2dcf20eb387df18bae627d9fa9a5ea6d8
9Rubi <1344760467@qq.com>
2020-01-18 00:34:28 +0800

    构建修改

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,14 @@ val kotlin_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.3.61"
+    id("com.github.johnrengelman.shadow") version "5.0.0"
 }
 
 group = "ink.rubi"
 version = "0.0.1-SNAPSHOT"
 
 application {
-    mainClassName = "io.ktor.server.netty.EngineMain"
+    mainClassName = "ink.rubi.DanmuFetcher"
 }
 
 repositories {
@@ -26,7 +27,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
+//    implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
@@ -34,11 +35,11 @@ dependencies {
     implementation("io.ktor:ktor-client-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-client-jackson:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("io.ktor:ktor-server-core:$ktor_version")
+//    implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-websockets:$ktor_version")
     implementation("io.ktor:ktor-client-websockets:$ktor_version")
     implementation("io.ktor:ktor-client-logging-jvm:$ktor_version")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+//    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
@@ -46,3 +47,13 @@ kotlin.sourceSets["test"].kotlin.srcDirs("test")
 
 sourceSets["main"].resources.srcDirs("resources")
 sourceSets["test"].resources.srcDirs("testresources")
+
+tasks.withType<Jar> {
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to application.mainClassName
+            )
+        )
+    }
+}
